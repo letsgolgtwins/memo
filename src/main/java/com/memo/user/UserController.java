776aa1a189f16ffadb1405599e6e83c8051d5a6a
LeeMinHee <98minh2@gmail.com>
2024-07-12 13:15:58 +0900

    240712 마크업

--- src/main/java/com/memo/user/UserController.java
+++ src/main/java/com/memo/user/UserController.java
@@ -15,4 +15,11 @@ public class UserController {
 		// 가운데 레이아웃 조각만 내려주면 전체 레이아웃으로 구성된다.
 		return "user/signUp"; 
 	}
+	
+	// 로그인 화면
+	// http://localhost/user/sign-in-view
+	@GetMapping("/sign-in-view")
+	public String signInView() {
+		return "user/signIn";
+	}
 }
